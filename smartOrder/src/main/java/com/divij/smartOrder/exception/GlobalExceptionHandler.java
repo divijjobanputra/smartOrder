34052a513c7f1f02a36e0c10a08f70049b74ae84
divijjobanputra <divijjobanputra@gmail.com>
2026-04-17 23:53:11 +0530

    clear error message for validation

--- smartOrder/src/main/java/com/divij/smartOrder/exception/GlobalExceptionHandler.java
+++ smartOrder/src/main/java/com/divij/smartOrder/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.divij.smartOrder.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -13,5 +14,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex){
         ErrorResponse err = new ErrorResponse(404, ex.getMessage());
         return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFound(MethodArgumentNotValidException ex){
+        ErrorResponse err = new ErrorResponse(400, ex.getBindingResult().getFieldError().getDefaultMessage());
+        return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
     }
 }
